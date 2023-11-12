1cc2e89b36b1eb233d73fd59435682774b87ad77
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2023-11-12 16:37:09 +0200

    denis commit 2

---
+++ main.php
@@ -0,0 +1,16 @@
+<?php
+  // Define the correct username and password
+  $correct_username = 'nbu';
+  $correct_password = 123;
+  
+  // Get the submitted username and password from the form
+  $submitted_username = $_POST['email'];
+  $submitted_password = $_POST['password'];
+  
+  // Check if the submitted username and password are correct
+  if ($submitted_username == $correct_username && $submitted_password == $correct_password) {
+      // Redirect to a success page
+      header("Location: entering-page.html");
+      exit();
+  } 
+  ?>
