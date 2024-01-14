fe63221e5d4b3c166373c00f1d5dd1c1c78a73a3
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2024-01-14 14:58:47 +0200

    adding php files for singin login ect

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+  ?>
+
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sozial</title>
+  </head>
+  <body>
+    <a href="logout.php">Logout</a>
+    <br>
+    Hello, Username.
+    
+  </body>
+  </html>
